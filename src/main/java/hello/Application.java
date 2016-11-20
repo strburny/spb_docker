cf97302900a7db72ad970303bb2737686f69a011
strburny <vadim.novikov@outlook.com>
2016-11-20 21:40:23 +0300

    initial commit

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,24 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Vadim on 11/19/2016.
+ */
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello Docker World";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
